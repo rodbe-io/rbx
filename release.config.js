0310d9be1294b14c72d572279125d64f76b4d2ad
kevinrodbe <kevinrodbe@users.noreply.github.com>
2025-01-21 12:24:37 -0500

    Initial commit

---
+++ release.config.js
@@ -0,0 +1,53 @@
+// eslint-disable-next-line no-restricted-exports
+export default {
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'main',
+    'master',
+    'next-major',
+    'next',
+    {
+      name: 'beta',
+      prerelease: true,
+    },
+    {
+      name: 'alpha',
+      prerelease: true,
+    },
+    {
+      name: 'pre',
+      prerelease: true,
+    },
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+        },
+        preset: 'conventionalcommits',
+        presetConfig: {
+          changelogHeader: '#RBX_PROJECT_NAME',
+          compareUrlFormat: '{{host}}/{{owner}}/{{repository}}/compare/{{previousTag}}...{{currentTag}}',
+          issuePrefixes: ['#'],
+          issueUrlFormat: '{{host}}/{{owner}}/{{repository}}/bug/{{id}}',
+        },
+      },
+    ],
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: true,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        message: 'release: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
